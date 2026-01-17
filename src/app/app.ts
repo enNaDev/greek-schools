439d75a00d86260c5b146450c6306ac523c62575
Enis Natzi <enis.natzi@gmail.com>
2026-01-17 13:28:11 +0200

    setup service to get the school list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { School, SchoolListService } from './school-list.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  private readonly schoolListService = inject(SchoolListService);
+
   protected readonly title = signal('greek-schools');
+  protected readonly schools = signal<School[]>([]);
+
+  ngOnInit() {
+    this.schoolListService.getSchools().subscribe((res) => this.schools.set(res));
+  }
 }
---
+++ src/app/school-list.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { tap } from "rxjs";
+
+export interface School {
+  area: string;
+  district: string;
+  email: string;
+  fax_number: string;
+  lat: number;
+  lng: number;
+  municipal_unit: string;
+  municipality: string;
+  phone_number: string;
+  prefecture: string;
+  regional_unit: string;
+  school_code: string;
+  school_district: null;
+  school_name: string;
+  school_subtype: string;
+  school_type: string;
+  street_address: string;
+  zip_code: string;
+}
+
+Injectable({ providedIn: 'root' })
+export class SchoolListService {
+  private readonly http = inject(HttpClient);
+
+  getSchools() {
+    return this.http.get<School[]>('https://data.gov.gr/api/v1/query/minedu_schools').pipe(tap((res) => console.log(res)));
+  }
+}
